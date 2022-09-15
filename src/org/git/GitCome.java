4bd16999c9f78a731c6e4a8eac7092ca449c61aa
Testervinoth <biriyanimutton2022@gmail.com>
2022-09-15 10:29:24 +0530

    Done

---
+++ src/org/git/GitCome.java
@@ -0,0 +1,15 @@
+package org.git;
+
+public class GitCome {
+	
+	private void vanakam() {
+         
+		System.out.println("gogogo");
+		
+	}
+	public static void main(String[] args) {
+		GitCome h=new GitCome();
+		h.vanakam();
+	}
+
+}
